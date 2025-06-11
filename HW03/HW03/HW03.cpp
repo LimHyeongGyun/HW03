14a13d62ccca3215b6e77b99ff03c5bc005b4320
LimHyeonGyun <200hk@naver.com>
2025-06-11 18:26:17 +0900

    도전기능 추가구현
    .

--- HW03/HW03/HW03.cpp
+++ HW03/HW03/HW03.cpp
@@ -4,10 +4,26 @@
 
 using namespace std;
 
-
 int main() {
-    
+
     SimpleVector<int> vec;
+
+    for (int i = 23; i > 0; i--) {
+        vec.push_back(i);
+    }
+    vec.sortData();
+    for (int i = 0; i < vec.size(); i++) {
+        cout << i << "번째: " << vec[i] << endl;
+    }
+    cout << vec.size() << endl;
+    cout << vec.capacity() << endl;
+    cout << "----------------------------------------------" << endl;
+    SimpleVector<int> cloneVec(vec);
+    for (int i = 0; i < cloneVec.size(); i++) {
+        cout << i << "번째: " << cloneVec[i] << endl;
+    }
+    cout << cloneVec.size() << endl;
+    cout << cloneVec.capacity() << endl;
 
     return 0;
 }
--- HW03/HW03/simpleVector.h
+++ HW03/HW03/simpleVector.h
@@ -1,22 +1,28 @@
 #ifndef SIMPLEVECTOR_H_
 #define SIMPLEVECTOR_H_
 
+#include <algorithm>
+
+using namespace std;
+
 template <typename T>
 class SimpleVector {
 private:
-    T* data;
     int currentSize = 0; //현재 원소의 개수
     int currentCapacity = 0; //현재 내부 배열의 크기
 
-	void resize();
+	void resize(int newCapacity);
 public:
+    T* data;
     SimpleVector();
     SimpleVector(int capacity);
+	SimpleVector(const SimpleVector& other);
 
     void push_back(const T& value);
     void pop_back();
     int size();
     int capacity();
+	void sortData();
 
     ~SimpleVector();
 
@@ -25,35 +31,43 @@ public:
 };
 
 template <typename T>
-SimpleVector<T>::SimpleVector() {
-	data = new T[currentCapacity = 10];
+SimpleVector<T>::SimpleVector() : currentSize(0), currentCapacity(10) {
+	data = new T[currentCapacity];
 }
 
 template <typename T>
-SimpleVector<T>::SimpleVector(int capacity) {
-	data = new T[currentCapacity = capacity];
+SimpleVector<T>::SimpleVector(int capacity) : currentSize(0), currentCapacity(capacity){
+	data = new T[currentCapacity];
 }
-
 template <typename T>
-void SimpleVector<T>::resize() {
-
-	//깊은 복사를 통한 resize
-
-	int cloneCapacity = currentCapacity + 2; //2배씩 크기가 증가하도록
-	T* cloneData = new T[cloneCapacity];
-
-	//data[i]가 쓰레기 값일 때의 버퍼 오버런 방지
-	if (data != nullptr) {
-		//새 배열로 복사하기
-		for (int i = 0; i < currentSize; i++) {
-			cloneData[i] = data[i];
-		}
+SimpleVector<T>::SimpleVector(const SimpleVector& other)
+: currentSize(other.currentSize), currentCapacity(other.currentCapacity){
+	data = new T[currentCapacity];
+	for (int i = 0; i < currentSize; i++) {
+		data[i] = other.data[i];
 	}
+}
+template <typename T>
+void SimpleVector<T>::resize(int newCapacity) {
 
-	delete[] data; //기존 배열 메모리 해제
+	//현재 배열보다 크기가 클때만 작동
+	if (newCapacity > currentCapacity) {
+		//깊은 복사를 통한 resize
+		T* cloneData = new T[newCapacity];
 
-	data = cloneData; //새 배열을 기존 data가 가리키도록 해줌
-	currentCapacity = cloneCapacity;
+		//data[i]가 쓰레기 값일 때의 버퍼 오버런 방지
+		if (data != nullptr) {
+			//새 배열로 복사하기
+			for (int i = 0; i < currentSize; i++) {
+				cloneData[i] = data[i];
+			}
+		}
+
+		delete[] data; //기존 배열 메모리 해제
+
+		data = cloneData; //새 배열을 기존 data가 가리키도록 해줌
+		currentCapacity = newCapacity;
+	}
 }
 
 template <typename T>
@@ -61,7 +75,7 @@ void SimpleVector<T>::push_back(const T& value) {
 
 	//배열의 크기보다 원소의 개수가 많아질 때
 	if (currentSize >= currentCapacity) {
-		resize(); //배열의 크기 조정
+		resize(currentCapacity + 5); //push_back에서는 배열의 크기 5 증가
 	}
 
 	data[currentSize++] = value;
@@ -87,6 +101,11 @@ template <typename T>
 int SimpleVector<T>::capacity() {
 
 	return currentCapacity;
+}
+
+template <typename T>
+void SimpleVector<T>::sortData() {
+	sort(data, data + currentSize);
 }
 
 template <typename T>
